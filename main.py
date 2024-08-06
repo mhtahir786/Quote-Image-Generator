b7b60e748288848fa5ee9fbe4498a1064a0ab4fe
Hamza <158337919+mhtahir786@users.noreply.github.com>
2024-08-06 18:35:32 +0100

    Image generation with quote placement, in Development Version 0.0.2

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from my_functions import *
 import requests
 import pandas as pd
 
+# IMPORTANT - Before use check these, or expect problems!
+# Ensure proxies are up to date {my_function.py/get_with_proxy/proxyList} - 'https://free-proxy-list.net/'
+# Ensure API functionality, my free account has a LIMIT of 50 requests {route to api key} - 'https://unsplash.com/'
+# Font is freeware, acquired from - 'https://www.fontspace.com/ora-urus-font-f119179'
+
 # Initialise necessary html data for quotes website
 url = "https://quotes.toscrape.com/"
-result = get_with_proxy(url)  # REMINDER - update proxy list when using
+result = get_with_proxy(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
 # # Collect relevant quote information
@@ -27,5 +32,14 @@ doc = BeautifulSoup(result.text, "html.parser")
 allQuotes = get_all_quote_data(url)
 # Get a random quote
 randomQuote = select_random_row(allQuotes)
-# Display quote data
-print("Quote = ", randomQuote['Quote'], "Tags = ", randomQuote['Tag'])
+# # Display quote data
+# print("Quote = ", randomQuote['Quote'], "Tags = ", randomQuote['Tag'])
+
+# # Generate an image using quote data
+# imageTag = randomQuote['Tag']
+# imageURL = generate_image_url(imageTag)
+# print(imageURL)
+
+# Test quote placed on image
+image = get_image_with_text(randomQuote['Quote'], randomQuote['Tag'])
+image.show()  # only shows and not save, so screenshot if needed
--- my_functions.py
+++ my_functions.py
@@ -1,18 +1,38 @@
 # All functions to be used in main program, placed here for better readability
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont  # PythonImageLibrary
+from io import BytesIO
 import requests
 import random
+import logging
+import textwrap
+import os
 
+
+# - - Setup for functions
+# Log's configuration, to track and debug in development
+logging.basicConfig(
+    filename='test.log',
+    filemode='a',  # append to log file
+    level=logging.DEBUG,  # set logging level (debugging, info, warning, error, critical)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' # time, name, level, message
+)
+
+# Load environment variables from file path specified
+load_dotenv(dotenv_path='store.env')
+
+# - - All functions
 # Uses a proxy to redirect data from site request
 def get_with_proxy(url):
     # Proxies attained from 'https://free-proxy-list.net/'
     # Remember to update proxies (ensure they work)
     proxyList = [
-        'http://194.5.25.34:443',        # Singapore
-        'http://20.111.54.16:8123',      # Canada
-        'http://160.86.242.23:8080',     # Japan
-        'http://8.223.31.16:1080',       # Singapore
-        'http://135.148.171.194:18080'   # USA
+        'http://128.199.136.56:3128',    # Singapore
+        'http://172.183.241.1:8090',     # USA
+        'http://189.240.60.166:9090',    # Mexico
+        'http://103.152.232.162:8199',     # Columbia
+        'http://223.135.156.183:8080'    # Japan
         ]
     
     # Loop through all proxies till one works
@@ -22,6 +42,7 @@ def get_with_proxy(url):
         try:  # attempt to use the proxy's available
             result = requests.get(url, proxies={'http': proxy, 'https': proxy})
             if result.status_code == 200:
+                logging.debug(f"Proxy used - {proxy}")
                 return result
             else:
                 print(f"This proxy failed - {proxy}, status code: {result.status_code}")
@@ -144,3 +165,111 @@ def select_random_row(quoteDict):
     keys = list(quoteDict.keys())  # convert dictionary keys to a list
     randomKey = random.choice(keys) # get a random key
     return quoteDict[randomKey]  # return row associated with that key
+
+
+# Returns an image URL generated using the Unsplash API given a string of tags
+def generate_image_url(tags):
+    # Grab API key from .env file, report issue when not found
+    apiKey = os.getenv('UNSPLASH_ACCESS_KEY')
+    if not apiKey:
+        logging.debug("Unable to get UNSPLASH_ACCESS_KEY from file")
+        raise ValueError("Unable to retrieve UNSPLASH_ACCESS_KEY")
+    
+    # Ensure tag parameter contains content
+    if tags:
+        logging.debug("Parameters contain content in function; generate_image_url")
+    else:
+        logging.debug("Error - No content in parameters for function; generate_image_url")
+        raise ValueError("Error - No content in parameters for function; generate_image_url")
+    
+    # Initiate a get request using tags given as parameter
+    apiURL = "https://api.unsplash.com/photos/random"
+    headers = {
+        'Authorization': f'Client-ID {apiKey}'
+    }
+    params = {
+        'query': tags,
+        'count': 1  # only one result required
+    }
+    result = requests.get(apiURL, headers=headers, params=params)
+
+    # Check process was successful and return image url
+    imageURL = "No image found"  # default issue
+    if result.status_code == 200:
+        data = result.json()
+        # Ensure data was returned and is not empty
+        if data:
+            imageURL = data[0]['urls']['regular']  # check API documentation for parsing details
+            logging.debug(f"Successfully obtained image url {data[0]['urls']['regular']}")
+        else:
+            logging.debug("Data returned from API was empty.")
+    else:
+        logging.debug(f"Error with API request statsus code {result.status_code} - {result.text}")
+    
+    return imageURL
+
+# Returns an image which has been downlaoded from url provided as parameter
+def download_image_from_url(url):
+    # Issue a get request for the image and check it's success
+    result = requests.get(url)
+    if result.status_code == 200:
+        # Return as an image in memory without saving it (for temporary use)
+        image = Image.open(BytesIO(result.content))
+        logging.debug("Successfully downlaoded image in function; download_image_from_url")
+        return image
+    else:
+        logging.debug(f"Couldn't downlaod image in function; download_image_from_url), Status code - {result.status_code}")
+        raise Exception(f"Couldn't downlaod image in function; download_image_from_url), Status code - {result.status_code}")
+    
+
+# Returns font size adjusted to fit a bounding box for the quote
+def draw_text_on_image(quoteText, image):
+    # Initialise font size & type ideally
+    FONT_NAME = "fonts/OraUrus-BLjPx.ttf"
+    FONT_SIZE = 120  # also max font size
+    FONT_COLOUR = (255,255,255)  # white
+    BOX_WIDTH = 900  # using static pixels over percentage sizes makes text appear different
+    BOX_HEIGHT = 600 # .. size with different image dimensions, this is an artistic choice
+
+    # Setup pillow library to draw on to image
+    fontSize = FONT_SIZE
+    draw = ImageDraw.Draw(image)
+
+    # Adjust font size dynamically till it fits bounding box
+    while True:
+        # Wrap text within bounding box by trying with current font size
+        font = ImageFont.truetype(FONT_NAME, fontSize)  # load font
+        wrapText = textwrap.fill(quoteText, width=BOX_WIDTH  // font.getbbox(' ')[1])  # returns as (left, top, right, bottom)
+
+        # Recalculate text with and height
+        left, top, right, bottom = draw.multiline_textbbox((0,0), wrapText, font=font)  # get bounding box, positions can be 0
+        textWidth = right - left
+        textHeight = bottom - top
+        logging.debug(f"Font Size - {fontSize}, Text Width - {textWidth}, Text Height - {textHeight}")
+
+        # Check text fits in bounding box and use that font, otherwise decrement and continue loop
+        if (textWidth <= BOX_WIDTH) and (textHeight <= BOX_HEIGHT):
+            break
+        fontSize -= 1
+    
+    # Use image and text dimensions to find the correct positions for text placement in CENTRE
+    imageWidth, imageHeight = image.size
+    posX = ((imageWidth - textWidth) / 2)
+    posY = ((imageHeight - textHeight) / 2)
+    logging.debug(f"Pos x - {posX}, Pos y - {posY}")
+    draw.multiline_text((posX, posY), wrapText, fill=FONT_COLOUR, font=font, align="center")
+
+    return image
+
+# Returns an image with quote text inserted on it
+def get_image_with_text(quoteText, quoteTags):    
+    # Obtain an image from API & download into memory for use
+    imageURL = generate_image_url(quoteTags) 
+    
+    # Download image temporarily into memory to draw on
+    image = download_image_from_url(imageURL)
+
+    # Draw text on to image using dynamic sizing and center placement onto image
+    image = draw_text_on_image(quoteText, image)   
+
+    return image
